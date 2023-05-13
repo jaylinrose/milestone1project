65474dbd0f145ab0dd520e1db0f1d9b8ebc0018d
jaylinrose <jaylin.manuel02@gmail.com>
2023-05-12 17:11:51 -0700

    added dialouge for gameintro page array and functions

--- index.js
+++ index.js
@@ -6,36 +6,3 @@ fadeOutButton.onclick = function(){
    box.classList.remove("fade-out")
    window.location.href='gameIntro.html';
 }
-
-var dialogs = ['Hello, i was so bored that i ended making this shit so yeah i am bored, as you can see i am bored !'],  
-    initial = 0;
-    individual = dialogs[initial].split('');
-
-function createDiag ( dialog ) {
-
-  for(i = 0; i < dialog.length; i++) {
-    (function(i){
-        
-      setTimeout(function(){
-        $('#dialog').text($('#dialog').text() +   dialog[i]);
-        if (i == dialog.length-1 ) {
-          $('#dialog').prepend('<div id="arrow"></div>');
-          Mousetrap.bind('enter', function() {
-            if (dialogs[initial+1]) {
-              $('#dialog').text('');
-              initial += 1;
-              individual = dialogs[initial].split('');
-              createDiag( individual );
-            }
-          });
-        }
-      }, 50*i);  
-    
-    }(i));
-    
-  }
-
-}
-
-createDiag( individual );
-
